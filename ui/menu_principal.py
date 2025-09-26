d3d395e951f27cb24477eee4f495ca6dc7c92326
baileytorch <baileytorch@hotmail.com>
2025-09-26 18:17:15 -0300

    Agregando modulo auxiliares

--- ui/menu_principal.py
+++ ui/menu_principal.py
@@ -1,6 +1,9 @@
+from auxiliares.version import version
+
+
 def menu_principal():
-    print('Sistema de Gestión de Usuarios')
-    print('==============================')
+    print(f'Sistema de Gestión de Usuarios v.{version}')
+    print('====================================')
     print('[1] Opción 1')
     print('[2] Opción 2')
     print('[3] Opción 3')
